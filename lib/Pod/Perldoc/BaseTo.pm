e35c8afa2ce06f7a8cce9783477de0272abb0ee9
Mathieu Arnold <mat@FreeBSD.org>
2015-04-26 22:23:06 +0200

    FreeBSD has has mandoc for quite a few years already.

--- lib/Pod/Perldoc/BaseTo.pm
+++ lib/Pod/Perldoc/BaseTo.pm
@@ -33,6 +33,7 @@ BEGIN {
  *is_linux   = $^O eq 'linux'    ? \&TRUE : \&FALSE unless defined &is_linux;
  *is_hpux    = $^O =~ m/hpux/    ? \&TRUE : \&FALSE unless defined &is_hpux;
  *is_openbsd = $^O =~ m/openbsd/ ? \&TRUE : \&FALSE unless defined &is_openbsd;
+ *is_freebsd = $^O =~ m/freebsd/ ? \&TRUE : \&FALSE unless defined &is_freebsd;
  *is_bitrig = $^O =~ m/bitrig/ ? \&TRUE : \&FALSE unless defined &is_bitrig;
 }
 

0479a88e71105c475f365dc186be58139ac1cdf5
Sawyer X <xsawyerx@cpan.org>
2017-11-29 14:34:45 +0200

    Sync all OS detection lines

--- lib/Pod/Perldoc.pm
+++ lib/Pod/Perldoc.pm
@@ -70,6 +70,9 @@ BEGIN {
  *is_linux   = $^O eq 'linux'   ? \&TRUE : \&FALSE unless defined &is_linux;
  *is_hpux    = $^O =~ m/hpux/   ? \&TRUE : \&FALSE unless defined &is_hpux;
  *is_amigaos = $^O eq 'amigaos' ? \&TRUE : \&FALSE unless defined &is_amigaos;
+ *is_openbsd = $^O =~ m/openbsd/ ? \&TRUE : \&FALSE unless defined &is_openbsd;
+ *is_freebsd = $^O =~ m/freebsd/ ? \&TRUE : \&FALSE unless defined &is_freebsd;
+ *is_bitrig = $^O =~ m/bitrig/ ? \&TRUE : \&FALSE unless defined &is_bitrig;
 }
 
 $Temp_File_Lifetime ||= 60 * 60 * 24 * 5;

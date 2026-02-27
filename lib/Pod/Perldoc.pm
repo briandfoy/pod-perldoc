74a430203de5bc343ab294fbbfc6b98fac8759cb
brian d foy <briandfoy@pobox.com>
2026-02-27 05:20:18 -0500

    Add PERLDOC_ROFFER to set the one I want to test

--- lib/Pod/Perldoc.pm
+++ lib/Pod/Perldoc.pm
@@ -488,7 +488,10 @@ sub init {
 sub _roffer_candidates {
     my( $self ) = @_;
 
-    if( $self->is_openbsd || $self->is_freebsd || $self->is_bitrig || $self->is_midnightbsd ) { qw( mandoc groff nroff ) }
+	if( defined $ENV{'PERLDOC_ROFFER'} ) {
+		( split /\s+/, $ENV{'PERLDOC_ROFFER'} );
+		}
+    elsif( $self->is_openbsd || $self->is_freebsd || $self->is_bitrig || $self->is_midnightbsd ) { qw( mandoc groff nroff ) }
     else                    { qw( groff nroff mandoc ) }
     }
 
